89cd6b08448c05b3b137436895e81d717ba284d0
Rishi Saxena <rishisaxenakvnkj@gmail.com>
2024-12-15 13:13:03 +0530

    Set up protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Users from './pages/Users'
+import Tasks from './pages/Tasks'
+import Trash from './pages/Trash'
+import TaskDetails from './pages/TaskDetails'
+import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom"
+import Toaster from 'sonner';
+
+function Layout(){
+  const user = "";
+  const location = useLocation()
+  return user ? (
+<div className='w-full h-screen flex flex-col md:flex-row'>
+  <div className='h-screen w-1/5 bg-white sticky top-0 hidden md:block'>
+ {/* <Sidebar /> */}
+  </div>
+  {/* <MobileSidebar /> */}
+  <div className='flex-1 overflow-y-auto'>
+    {/* <Navbar /> */}
+    <div className='p-4 2xl:px-10'>
+      <Outlet />
+    </div>
+  </div>
+</div>
+  ):(
+    <Navigate to="/log-in" state={{from : location}} replace />
+  )
+
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  return ( <main className="w-full min-h-screen bg-[#f3f4f6]">
+    <Routes>
+      <Route element={<Layout />}>
+      <Route path="/" element={<Navigate to="/dashboard"/>} />
+      <Route path='/dashboard' element={<Dashboard />}/>
+      <Route path='/tasks' element={<Tasks />}/>
+      <Route path='/completed/:status' element={<Dashboard />}/>
+      <Route path='/in-progress/:status' element={<Dashboard />}/>
+      <Route path='/todo/:status' element={<Dashboard />}/>
+      <Route path='/team' element={<Users />}/>
+      <Route path='/trashed' element={<Trash />}/>
+      <Route path='/task/:id' element={<TaskDetails/>} />
+      </Route>
+      <Route path='/log-in' element={<Login/>} />
+    </Routes>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Toaster richColors />
+  </main>)
 }
 
 export default App
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <main className="w-full min-h-screen bg-[#f3f4f6]">
+      <Routes>
+        
+      </Routes>
+    </main>
+  );
+};
+
+export default Login;
